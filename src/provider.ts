eb333bce18357b136621a2d9079bc00fe40a09aa
Eike Christian Karbe <eichkarbe@yahoo.de>
2019-12-05 19:16:00 +0100

    initial commit

---
+++ src/provider.ts
@@ -0,0 +1,50 @@
+import * as vscode from 'vscode';
+import * as reader from './reader';
+
+export class RSSProvider implements vscode.TreeDataProvider<any> {
+    private _onDidChangeTreeData: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
+    readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
+
+    public title: string | undefined;
+    public link: string | undefined;
+    public description: string | undefined;
+
+    constructor() {
+    }
+
+    refresh(): void {
+        this._onDidChangeTreeData.fire();
+    }
+
+    getTreeItem(element: reader.IEntry): vscode.TreeItem {
+        return {
+            label: element.title,
+            tooltip: element.title,
+            collapsibleState: vscode.TreeItemCollapsibleState.None,
+            command: {
+                command: 'RSS.OpenArticle',
+                title: '',
+                arguments: [element.link]
+            }
+        };
+    }
+
+    getChildren(element?: any): Promise<Array<reader.IEntry>> {
+        return new Promise<any>((resolve, reject) => {
+            reader.XML("https://heise.de/rss/heise-atom.xml")
+                .then(response => {
+                    resolve(response.entries);
+                });
+        });
+    }
+}
+
+export class Article extends vscode.TreeItem {
+    constructor(
+        public readonly label: string,
+        public readonly collapsableState: vscode.TreeItemCollapsibleState,
+        public readonly command: vscode.Command
+    ) {
+        super(label, collapsableState);
+    }
+}
---
+++ src/reader.ts
@@ -0,0 +1,142 @@
+import * as hm from 'typed-rest-client/HttpClient';
+
+const http: hm.HttpClient = new hm.HttpClient('XML');
+
+export interface IFeed {
+    title: string;
+    entries: Array<IEntry>;
+}
+
+export interface IEntry {
+    title: string;
+    link: string;
+    summary: string;
+}
+
+export async function XML(URL: string): Promise<IFeed> {
+    return new Promise<any>((resolve, reject) => {
+        http.get(URL)
+            .then(res => {
+                res.readBody()
+                    .then(body => {
+                        let feed = parseXML(body);
+                        resolve(feed);
+                    });
+            });
+    });
+}
+
+function parseXML(XML: string): IFeed {
+    if (XML.match(/\<feed xmlns=\"http:\/\/www\.w3\.org\/2005\/Atom\"/g)) {
+        let items = XML.split('<entry>');
+        let feed: IFeed = {
+            title: "",
+            entries: [],
+        };
+        let titleArr = items[0].split('</title>');
+        let re: RegExp = RegExp("<title.*>(.*)", "gm");
+        let title = re.exec(titleArr[0]);
+        if (title) {
+            feed.title = title[1];
+        }
+        items.splice(0, 1);
+        items.forEach(element => {
+            let entry: IEntry = {
+                title: "",
+                link: "",
+                summary: "",
+            };
+            re = RegExp("<title.*>(.*)(?=<\/title>)", "gm");
+            title = re.exec(element);
+            if (title) {
+                entry.title = title[1];
+            }
+            re = RegExp('<link href="(.*)(?="\/>)', "gm");
+            let link = re.exec(element);
+            if (link) {
+                entry.link = link[1];
+            }
+            re = RegExp("<summary.*>(.*)(?=<\/summary>)", "gm");
+            let summary = re.exec(element);
+            if (summary) {
+                entry.summary = summary[1];
+            }
+            feed.entries.push(entry);
+        });
+        return feed;
+    } else if (XML.match(/<rss version="2\.0"/g)) {
+        let items = XML.split('<item>');
+        let feed: IFeed = {
+            title: "",
+            entries: [],
+        };
+        let titleArr = items[0].split('</title>');
+        let re: RegExp = RegExp("<title>(.*)", "gm");
+        let title = re.exec(titleArr[0]);
+        if (title) {
+            feed.title = title[1];
+        }
+        items.splice(0, 1);
+        items.forEach(element => {
+            let entry: IEntry = {
+                title: "",
+                link: "",
+                summary: "",
+            };
+            re = RegExp("<title>(.*)(?=<\/title>)", "gm");
+            title = re.exec(element);
+            if (title) {
+                entry.title = title[1];
+            }
+            re = RegExp("<link>(.*)(?=<\/link>)", "gm");
+            let link = re.exec(element);
+            if (link) {
+                entry.link = link[1];
+            }
+            re = RegExp("<description>(.*)(?=<\/description>)", "gm");
+            let summary = re.exec(element);
+            if (summary) {
+                entry.summary = summary[1];
+            }
+            feed.entries.push(entry);
+        });
+        return feed;
+    } else {
+        let items = XML.split('<item>');
+        let feed: IFeed = {
+            title: "",
+            entries: [],
+        };
+        let titleArr = items[0].split('</title>');
+        let re: RegExp = RegExp("<title>(.*)", "gm");
+        let title = re.exec(titleArr[0]);
+        if (title) {
+            feed.title = title[1];
+        }
+        items.splice(0, 1);
+        items.forEach(element => {
+            let entry: IEntry = {
+                title: "",
+                link: "",
+                summary: "",
+            };
+            re = RegExp("<title>(.*)(?=<\/title>)", "gm");
+            title = re.exec(element);
+            if (title) {
+                entry.title = title[1];
+            }
+            re = RegExp('<source url="(.*)(?=">)', "gm");
+            let link = re.exec(element);
+            if (link) {
+                entry.link = link[1];
+            }
+            re = RegExp("<description>(.*)(?=<\/description>)", "gm");
+            let summary = re.exec(element);
+            if (summary) {
+                entry.summary = summary[1];
+            }
+            feed.entries.push(entry);
+        });
+        return feed;
+    }
+}
